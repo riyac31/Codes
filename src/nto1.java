c72169b1b3c7426d2078a5e47e342f3d91dbba25
riyac31 <riyachoudhary3105@gmail.com>
2022-07-22 20:09:21 +0530

    jul18

--- src/nto1.java
+++ src/nto1.java
@@ -1,15 +1,4 @@
 import java.util.*;
 public class nto1 {
-    static void print(int n){
-        if(n>5)
-            return;
-        System.out.println(n);
-        n++;
-        print(n);
-    }
 
-    public static void main(String[] args) {
-        int n = 1;
-        print(n);
-    }
 }
